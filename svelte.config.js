d8d76485a4a2dc48a8f9fff1b8354cd344acba40
AlvareYN <alvarotomasdlamaza@hotmail.com>
2025-08-15 13:38:10 -0400

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,21 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-
 export default {
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: null
+      fallback: undefined,
+      precompress: false,
+      strict: true
     }),
     paths: {
       base: process.env.GITHUB_PAGES ? '/colors' : '',
     },
     prerender: {
       default: true
-    }
+    },
   },
   preprocess: vitePreprocess(),
 };
